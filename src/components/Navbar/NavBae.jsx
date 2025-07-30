07e9e14d0dc580a154cd95ad255e40eee336292a
Nur Mohammod <nurm98371@gmail.com>
2025-07-30 20:19:28 +0600

    toggole

--- src/components/Navbar/NavBae.jsx
+++ src/components/Navbar/NavBae.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import Link from '../Link/Link';
-
+import React, { useState } from "react";
+import Link from "../Link/Link";
+import { AiOutlineMenuFold } from "react-icons/ai";
 const NavBar = () => {
-    const routes=[
-        {id:1,path:'/',name:'Home'},
-        {id:2,path:'/about',name:'About'},
-        {id:3,path:'/services',name:'Services'},
-        {id:4,path:'/contact',name:'Contact'},
-        {id:5,path:'*',name:'NotFound'}
-    ];
+  const [open, setOpen] = useState(false);
+  const routes = [
+    { id: 1, path: "/", name: "Home" },
+    { id: 2, path: "/about", name: "About" },
+    { id: 3, path: "/services", name: "Services" },
+    { id: 4, path: "/contact", name: "Contact" },
+    { id: 5, path: "*", name: "NotFound" },
+  ];
 
   return (
-    <div className='md:flex'>
-      {
-        routes.map(route=><Link key={route.id} route={route}></Link>)
-      }
-      
+    <div className="md:flex">
+      <div className="md:hidden" onClick={() => setOpen(true)}>
+        {open === true ? "open" : "close"}
+        <AiOutlineMenuFold className="text-2xl " />
+      </div>
+      {routes.map((route) => (
+        <Link key={route.id} route={route}></Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
