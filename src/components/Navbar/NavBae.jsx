a815c5d9af427d16707e71933c5daad63422bf0d
Nur Mohammod <nurm98371@gmail.com>
2025-07-30 19:59:17 +0600

    Link

---
+++ src/components/Navbar/NavBae.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from '../Link/Link';
+
+const NavBar = () => {
+    const routes=[
+        {id:1,path:'/',name:'Home'},
+        {id:2,path:'/about',name:'About'},
+        {id:3,path:'/services',name:'Services'},
+        {id:4,path:'/contact',name:'Contact'},
+        {id:5,path:'*',name:'NotFound'}
+    ];
+
+  return (
+    <div className='md:flex'>
+      {
+        routes.map(route=><Link key={route.id} route={route}></Link>)
+      }
+      
+    </div>
+  )
+}
+
+export default NavBar
